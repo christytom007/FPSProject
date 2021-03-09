5a0d6992ae4516e8bacf456073a45e69f5e4b2a7
Christy Thomas <christytom.007@gmail.com>
2021-03-08 20:15:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FPSProject
+ FPS Game Base made in Unreal Engine 4 using C++
---
+++ Source/FPSProject/FPSCharacter.cpp
@@ -0,0 +1,107 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create FPS Camera component
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(FPSCameraComponent != nullptr);
+
+	//Attach the camera component to our capsule component
+	FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+
+
+	//Position the camera slightly above the eyes
+	FPSCameraComponent->SetRelativeLocation(FVector(0.0f,0.0f,50.0f+BaseEyeHeight));
+
+	//Enable the pawn to control the camera rotation.
+	FPSCameraComponent->bUsePawnControlRotation = true;
+
+	//Create a First Person mesh component for the owning player.
+	FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(FPSMesh != nullptr);
+
+	//Only the owing player see this mesh.
+	FPSMesh->SetOnlyOwnerSee(true);
+
+	//Attach the FPS mesh to FPS Camera
+	FPSMesh->SetupAttachment(FPSCameraComponent);
+
+	// Disable some environmental  shadow to preserve the illusion of having a single mesh.
+	FPSMesh->bCastDynamicShadow = false;
+	FPSMesh->CastShadow = false;
+
+	// The owning player doesn't see the regular (third-person) body mesh
+	GetMesh()->SetOwnerNoSee(true);
+
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Setup Movement Bindings.
+	PlayerInputComponent->BindAxis("MoveForward",this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+
+	//Setup "look" bindings.
+	PlayerInputComponent->BindAxis("Turn", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFPSCharacter::AddControllerPitchInput);
+
+	//Setup Action bindings.
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this , &AFPSCharacter::StopJump);
+	
+}
+
+void AFPSCharacter::MoveForward(float Value)
+{
+	// Find out which way is "Forward" and record that player want to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, Value);
+}
+
+void AFPSCharacter::MoveRight(float Value)
+{
+	// Find out witch way is "Right" and record that player want to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, Value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AFPSCharacter::StopJump()
+{
+	bPressedJump = false;
+}
+
